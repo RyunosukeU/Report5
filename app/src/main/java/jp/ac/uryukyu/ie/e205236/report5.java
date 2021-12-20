a65ea5c23422b1b39213671b913fcd7f821e803b
nosuke <ryuunosuke06160616@gmail.com>
2021-12-21 02:49:04 +0900

    step2 commit

--- app/src/main/java/jp/ac/uryukyu/ie/e205236/report5.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e205236/report5.java
@@ -2,7 +2,12 @@ package jp.ac.uryukyu.ie.e205236;
 
 public class report5 {
     public static void main(String[] args){
-        String str = null;
-        System.out.println(str.length());
+        try{
+            String str = null;
+            System.out.println(str.length());
+        }catch(NullPointerException e){
+            System.out.println("例外発生中");
+            System.out.println(e.getMessage());
+        }
     }
 }
