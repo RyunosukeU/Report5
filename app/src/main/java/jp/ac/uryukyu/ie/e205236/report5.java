777ee9287b46200358f1df612fd28bd4cb175fef
nosuke <ryuunosuke06160616@gmail.com>
2021-12-21 02:51:53 +0900

    step3 commit

--- app/src/main/java/jp/ac/uryukyu/ie/e205236/report5.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e205236/report5.java
@@ -2,12 +2,13 @@ package jp.ac.uryukyu.ie.e205236;
 
 public class report5 {
     public static void main(String[] args){
-        try{
-            String str = null;
-            System.out.println(str.length());
-        }catch(NullPointerException e){
+        String str = "百二十三";
+        try {
+            int value = Integer.parseInt(str);  
+        }catch(NumberFormatException e){
             System.out.println("例外発生中");
-            System.out.println(e.getMessage());
+        }finally{
+            System.out.println("処理終了");
         }
     }
 }
